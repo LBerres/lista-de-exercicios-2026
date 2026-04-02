f7e6ac958f150f0ab8380e507f8d39b9034a3183
Leandro Berres Pereira <leandro.motorhead@gmail.com>
2026-04-02 20:46:05 -0300

    Finalizado Exercicio02

--- Exercicio02.ConsoleApp/Program.cs
+++ Exercicio02.ConsoleApp/Program.cs
@@ -5,4 +5,37 @@
         a. A quilometragem inicial do veículo no início da viagem.
         b. A quilometragem final ao término da viagem.
         c. A quantidade de combustível consumida durante a viagem (em litros).
+    (Fórmula: Consumo por Km = Litros Abastecidos / Quilometragem Rodados)
 */
+
+Console.WriteLine("-------------------------------------------");
+Console.WriteLine("Calculando o Consumo de Combustível por Quilômetro");
+Console.WriteLine("-------------------------------------------");
+
+Console.Write("Digite a Quilometragem Inicial do Veículo (em km): ");
+double quilometragemInicial = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("-------------------------------------------");
+
+Console.Write("Digite a Quilometragem Final do Veículo (em km): ");
+double quilometragemFinal = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("-------------------------------------------");
+
+Console.Write("Digite a Quantidade de Combustível Consumido (em litros): ");
+double litrosConsumidos = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("-------------------------------------------");
+
+double distanciaTotal = quilometragemFinal - quilometragemInicial;
+
+if (litrosConsumidos > 0 && distanciaTotal > 0)
+{
+    double consumoPorKm = distanciaTotal / litrosConsumidos; // Consumo por Km = Quilometragem Rodados / Litros Abastecidos
+    Console.WriteLine($"O Consumo de Combustível por Quilômetro é: {consumoPorKm:F4} km/l"); // F = Floating-point format, 4 casas decimais
+}
+else
+{
+    Console.WriteLine("Erro: A quantidade de combustível consumida e a distância percorrida devem ser maiores que zero.");
+}
+
+Console.WriteLine("-------------------------------------------");
+Console.WriteLine("Pressione Enter para sair...");
+Console.ReadLine();
