6ce272724327773d2d820ab518e03f6fb31415e7
Leandro Berres Pereira <leandro.motorhead@gmail.com>
2026-04-02 19:41:22 -0300

    Adicionado Base de Calculo

--- Exercicio01.ConsoleApp/Program.cs
+++ Exercicio01.ConsoleApp/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 1. Crie um Programa para Calcular o Volume de uma Caixa Retangular
+// Multiplique a largura, altura e comprimento (Fórmula: V = L x A x C)
+// Base Para o Cálculo: 50 cm (comprimento) x 30 cm (largura) x 20 cm (altura).
+
+double comprimento = 50; // em cm
+double largura = 30; // em cm
+double altura = 20; // em cm
+
+double volume = comprimento * largura * altura; // V = L x A x C
+
+Console.WriteLine($"O Volume da Caixa Retangular é: {volume:F2} cm³"); // F = Floating-point format, 2 casas decimais
+
+Console.ReadLine();
