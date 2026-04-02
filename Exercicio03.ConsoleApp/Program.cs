5053b854a27ce1805fe04a515cb3f535ff175397
Leandro Berres Pereira <leandro.motorhead@gmail.com>
2026-04-02 20:49:19 -0300

    Adicionado Exercicio03

---
+++ Exercicio03.ConsoleApp/Program.cs
@@ -0,0 +1,4 @@
+﻿// 3. Crie um programa para converter a temperatura da escala Celsius para a escala Fahrenheit
+// Multiplicamos a temperatura em ºC por 1,8 e somamos 32 ao resultado.
+
+Console.ReadLine();
