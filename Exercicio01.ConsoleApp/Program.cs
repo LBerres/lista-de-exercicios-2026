4cdc2ed4441b1f584a01219d138e5ecae7795b7f
Leandro Berres Pereira <leandro.motorhead@gmail.com>
2026-04-02 19:46:29 -0300

    Finalizado o Exercício 01

--- Exercicio01.ConsoleApp/Program.cs
+++ Exercicio01.ConsoleApp/Program.cs
@@ -2,12 +2,26 @@
 // Multiplique a largura, altura e comprimento (Fórmula: V = L x A x C)
 // Base Para o Cálculo: 50 cm (comprimento) x 30 cm (largura) x 20 cm (altura).
 
-double comprimento = 50; // em cm
-double largura = 30; // em cm
-double altura = 20; // em cm
+Console.WriteLine("-------------------------------------------");
+Console.WriteLine("Calculando o Volume de uma Caixa Retangular");
+Console.WriteLine("-------------------------------------------");
+
+Console.Write("Digite o comprimento da caixa (em cm): ");
+double comprimento = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("-------------------------------------------");
+
+Console.Write("Digite a largura da caixa (em cm): ");
+double largura = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("-------------------------------------------");
+
+Console.Write("Digite a altura da caixa (em cm): ");
+double altura = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("-------------------------------------------");
 
 double volume = comprimento * largura * altura; // V = L x A x C
 
 Console.WriteLine($"O Volume da Caixa Retangular é: {volume:F2} cm³"); // F = Floating-point format, 2 casas decimais
 
+Console.WriteLine("-------------------------------------------");
+Console.WriteLine("Pressione Enter para sair...");
 Console.ReadLine();
