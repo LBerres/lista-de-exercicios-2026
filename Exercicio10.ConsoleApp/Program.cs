6087ca21493b2e75f6b1beb69d9e13ea38486c7c
Leandro Berres Pereira <leandro.motorhead@gmail.com>
2026-04-02 22:49:20 -0300

    Finalizado Exercicio10

--- Exercicio10.ConsoleApp/Program.cs
+++ Exercicio10.ConsoleApp/Program.cs
@@ -4,7 +4,33 @@ os números múltiplos de 3 pela palavra "Fizz" e os múltiplos de 5 pela palavr
 números que são múltiplos de ambos, use "FizzBuzz".
     a. Ex: 1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, FizzBuzz, ...
 */
+Console.WriteLine("-------------------------------------------");
+Console.WriteLine("Fizz! - Buzz! - FizzBuzz!");
+Console.WriteLine("-------------------------------------------");
+Console.WriteLine();
 
+Console.WriteLine("Aperte Enter para iniciar...");
+Console.ReadLine();
+
+for (int numero = 1; numero <= 100; numero++)
+{
+    if (numero % 3 == 0 && numero % 5 == 0)
+    {
+        Console.WriteLine("FizzBuzz, ");
+    }
+    else if (numero % 3 == 0)
+    {
+        Console.WriteLine("Fizz, ");
+    }
+    else if (numero % 5 == 0)
+    {
+        Console.WriteLine("Buzz, ");
+    }
+    else
+    {
+        Console.WriteLine($"{numero}, ");
+    }
+}
 
 Console.WriteLine("\n-------------------------------------------");
 Console.WriteLine("Pressione Enter para sair...");
