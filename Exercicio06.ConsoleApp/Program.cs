5e1bc57078bfce76638d5fc6063ad3a30891c834
Leandro Berres Pereira <leandro.motorhead@gmail.com>
2026-04-02 21:38:56 -0300

    Adicionado Exercicio06

---
+++ Exercicio06.ConsoleApp/Program.cs
@@ -0,0 +1,5 @@
+﻿// 6. Faça um algoritmo que leia os valores A, B, C e imprima na tela se a soma de A + B é menor que C.
+
+Console.WriteLine("-------------------------------------------");
+Console.WriteLine("Pressione Enter para sair...");
+Console.ReadLine();
