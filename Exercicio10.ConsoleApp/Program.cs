8b8d185f582ecb02c0a64b0414821247f085d7bc
Leandro Berres Pereira <leandro.motorhead@gmail.com>
2026-04-02 22:48:59 -0300

    Adicionado Exercicio10

---
+++ Exercicio10.ConsoleApp/Program.cs
@@ -0,0 +1,11 @@
+﻿/*
+10. Escreva um programa que imprima os números de 1 a 100 em ordem crescente, substituindo
+os números múltiplos de 3 pela palavra "Fizz" e os múltiplos de 5 pela palavra "Buzz". Para
+números que são múltiplos de ambos, use "FizzBuzz".
+    a. Ex: 1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, FizzBuzz, ...
+*/
+
+
+Console.WriteLine("\n-------------------------------------------");
+Console.WriteLine("Pressione Enter para sair...");
+Console.ReadLine();
